246f3cea6fcde19efdc0f21fc926f9f47eb00f74
apache13 <apache13@gmail.com>
2018-02-04 19:42:28 +0700

    update seed data and icon

--- db/seeds.rb
+++ db/seeds.rb
@@ -35,3 +35,24 @@ Iceland = Team.create(name: 'Iceland', code: 'IS', group: D)
 Croatia = Team.create(name: 'Croatia', code: 'HR', group: D)
 Nigeria = Team.create(name: 'Nigeria', code: 'NG', group: D)
 
+Brazil = Team.create(name: 'Brazil', code: 'BR', group: E)
+Switzerland = Team.create(name: 'Switzerland', code: 'CH', group: E)
+CostaRica = Team.create(name: 'Costa Rica', code: 'CR', group: E)
+Serbia = Team.create(name: 'Serbia', code: 'RS', group: E)
+
+Germany = Team.create(name: 'Germany', code: 'DE', group: F)
+Mexico = Team.create(name: 'Mexico', code: 'MX', group: F)
+Sweden = Team.create(name: 'Sweden', code: 'SE', group: F)
+SouthKorea = Team.create(name: 'South Korea', code: 'KR', group: F)
+
+Belgium = Team.create(name: 'Belgium', code: 'BE', group: G)
+Panama = Team.create(name: 'Panama', code: 'PA', group: G)
+Tunisia = Team.create(name: 'Tunisia', code: 'TN', group: G)
+England = Team.create(name: 'England', code: 'GB', group: G)
+
+Poland = Team.create(name: 'Poland', code: 'PL', group: H)
+Senegal = Team.create(name: 'Senegal', code: 'SN', group: H)
+Colombia = Team.create(name: 'Colombia', code: 'CO', group: H)
+Japan = Team.create(name: 'Japan', code: 'JP', group: H)
+
+
